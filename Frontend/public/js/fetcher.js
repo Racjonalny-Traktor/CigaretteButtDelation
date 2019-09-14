335d461ffe360fc0e91ecf849b6616014b517700
KonradusPL <konradus2012@gmail.com>
2019-09-14 19:11:50 +0200

    feat(api): added fetcher class

---
+++ Frontend/public/js/fetcher.js
@@ -0,0 +1,26 @@
+import { baseUrl } from "./utils.js";
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*',
+    'Origin': '*',
+    'Access-Control-Request-Headers': '*',
+    'Content-type': 'application/json'
+};
+class Fetcher{
+
+    postData(url, data){
+        return fetch(url, {
+            method:  'POST',
+            body: JSON.stringify(data),
+            headers:{'Content-Type': 'application/json'}
+        })
+    }
+
+    fetchLitter(){
+        const url = `${baseUrl}/api/Litter`;
+        return fetch(url, headers).then(response => response.json());
+    }
+}
+
+export default new Fetcher();
