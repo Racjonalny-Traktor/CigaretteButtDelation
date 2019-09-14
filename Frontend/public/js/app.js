aab9bedd90b1235633c62a2ebe3dc26c7bba4534
KonradusPL <konradus2012@gmail.com>
2019-09-15 01:56:29 +0200

    added sending test data to server

--- Frontend/public/js/app.js
+++ Frontend/public/js/app.js
@@ -1,7 +1,20 @@
 import map from './map.js';
 import fetcher from './fetcher.js';
 
-
-fetcher.fetchLitter().then(data =>{
+fetcher.fetchLitter().then( data =>{
     console.log(data);
+});
+
+map.on('click', (e) =>{
+    let lat = e.lngLat.lat;
+    let long = e.lngLat.lng;
+    let cigarettesNum = Math.floor(Math.random() * 10) %10
+    let pic = new File([''], '/assets/pety.jpg');
+
+    let data = {lat, long, cigarettesNum, pic};
+    fetcher.sendLitter(data).then(() =>{
+
+    }).catch(reason =>{
+        console.log(reason);
+    });
 })
