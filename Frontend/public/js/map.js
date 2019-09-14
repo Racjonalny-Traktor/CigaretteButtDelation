22d8f2e4ff9e436c27b7a17b75cc6f1613268351
KonradusPL <konradus2012@gmail.com>
2019-09-14 18:30:39 +0200

    chore: moved web files to public

--- Frontend/public/js/map.js
+++ Frontend/public/js/map.js
@@ -1,6 +1,9 @@
-
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3Bla2FsYSIsImEiOiJjazBqcDl0dXIwYzJxM2dzdmVoMGdsbmx3In0.LUI-WrcAUM_It4GHMo9hvQ';
 var map = new mapboxgl.Map({
     container: 'mapContainer',
-    style: 'mapbox://styles/mapbox/streets-v11'
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [20.831121, 52.109050],
+    zoom: 11
 });
+
+export default map;
