2b38d3e4a74eb3cfdf58475e210ed880b16e354f
KonradusPL <konradus2012@gmail.com>
2019-09-14 17:26:30 +0200

    feat(map): added mapbox's basic map

---
+++ Frontend/js/app.js
@@ -0,0 +1,6 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia3Bla2FsYSIsImEiOiJjazBqcDl0dXIwYzJxM2dzdmVoMGdsbmx3In0.LUI-WrcAUM_It4GHMo9hvQ';
+var map = new mapboxgl.Map({
+    container: 'mapContainer',
+    style: 'mapbox://styles/mapbox/streets-v11'
+});
