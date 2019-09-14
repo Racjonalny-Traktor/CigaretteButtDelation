2a4b6b5d824ee62c30f1f15c759a7a161b7a3648
Dominik Ruman <dr124@o2.pl>
2019-09-14 18:47:54 +0200

    Update Startup.cs

--- Backend/Startup.cs
+++ Backend/Startup.cs
@@ -28,6 +28,7 @@ namespace microserv
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("db")));
             services.AddSwaggerGen(c =>
