15287ff9e91414d2dd993c4587f1c0f0449c21cd
Dominik Ruman <dr124@o2.pl>
2019-09-14 19:38:27 +0200

    geojson

--- Backend/Controllers/LitterController.cs
+++ Backend/Controllers/LitterController.cs
@@ -1,9 +1,11 @@
-﻿using microserv.Models;
+﻿using Accord.MachineLearning;
+using microserv.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -45,7 +47,7 @@ namespace microserv.Controllers
 
             using (var stream = pic.OpenReadStream())
                 Task.WaitAll(blockBlob.UploadFromStreamAsync(stream));
-            
+
             data.ImageUrl = blockBlob.SnapshotQualifiedStorageUri.PrimaryUri.ToString();
             _context.Litters.Update(data);
             _context.SaveChanges();
@@ -57,6 +59,70 @@ namespace microserv.Controllers
         public IActionResult GetAllLitter()
         {
             return Ok(_context.Litters.ToArray());
+        }
+
+        [HttpGet("geojson")]
+        public IActionResult GetAsGeojson()
+        {
+            var data = _context.Litters.ToArray();
+
+            return Ok(new
+            {
+                type = "FeatureCollection",
+                crs = new
+                {
+                    type = "name",
+                    properties = new
+                    {
+                        name = "urn:ogc:def:crs:OGC:1.3:CRS84"
+                    }
+                },
+                features = data.Select((x, i) => new
+                {
+                    type = "Feature",
+                    properties = new
+                    {
+                        id = i,
+                        mag = x.CigarettesNum,
+                        time = DateTime.UtcNow.ToFileTime(),
+                    },
+                    geometry = new
+                    {
+                        type = "Point",
+                        coordinates = new[] { x.Long, x.Lat, x.CigarettesNum }
+                    }
+                })
+            });
+        }
+
+        [HttpGet("clustered/{count}")]
+        public IActionResult GetClustered(int count)
+        {
+            var data = _context.Litters.ToArray();
+
+            var kMeans = new KMeans(count);
+            var preparedData = data.Select(x => new double[] {
+                x.Lat.MapCoordToInt(),
+                x.Long.MapCoordToInt(),
+                x.CigarettesNum }
+            ).ToArray();
+           
+            var clusters = kMeans.Learn(preparedData);
+
+            return Ok(clusters.Centroids);
+        }
+    }
+
+    public static class CoordsExtensions
+    {
+        public static int MapCoordToInt(this double x)
+        {
+            return (int)Math.Round(x * 1000);
+        }
+
+        public static double MapIntToCoord(this int x)
+        {
+            return x / 1000.0;
         }
     }
 }
